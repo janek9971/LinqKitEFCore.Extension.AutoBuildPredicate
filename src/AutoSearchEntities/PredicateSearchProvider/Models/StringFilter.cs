ca6c1c5665ebeb12508c5aabaca5f4d8d1597c80
Krzysztof Jankowski <krzysztof.jankowski@ncplus.pl>
2019-10-25 09:18:58 +0200

    stringFIlter

---
+++ src/AutoSearchEntities/PredicateSearchProvider/Models/StringFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using AutoSearchEntities.PredicateSearchProvider.CustomUtilities.Enums;
+
+namespace AutoSearchEntities.PredicateSearchProvider.Models
+{
+   public class StringFilter
+   {
+       public string Str { get; set; }
+       public StringSearchOption StringSearchOption { get; set; } = StringSearchOption.Equals;
+       public StringComparison StringComparison { get; set; } = StringComparison.InvariantCultureIgnoreCase;
+   }
+
+
+}
