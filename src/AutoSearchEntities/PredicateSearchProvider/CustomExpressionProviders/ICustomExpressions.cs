4d0ed44a54e98d8162a2815e70626d37e22cd73f
Krzysztof Jankowski <krzysztof.jankowski@ncplus.pl>
2019-10-29 12:50:49 +0100

    removed adding customExpressions

--- src/AutoSearchEntities/PredicateSearchProvider/CustomExpressionProviders/ICustomExpressions.cs
+++
@@ -1,9 +0,0 @@
-﻿#pragma warning disable 1591
-
-namespace AutoSearchEntities.PredicateSearchProvider.CustomExpressionProviders
-{
-    public interface ICustomExpressions<TEntity> where TEntity : class
-    {
-         void Expressions(ModelExpressions<TEntity>.ExpressionsBuilder builder);
-    }
-}
