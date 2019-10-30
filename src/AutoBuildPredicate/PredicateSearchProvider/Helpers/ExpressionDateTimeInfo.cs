1dcd2d878f4cc92a11a3c7e783676e2d2f287db6
Krzysztof Jankowski <krzysztof.jankowski@ncplus.pl>
2019-10-30 13:51:28 +0100

    added ExpressionDateTimeInfo

---
+++ src/AutoBuildPredicate/PredicateSearchProvider/Helpers/ExpressionDateTimeInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq.Expressions;
+using AutoBuildPredicate.PredicateSearchProvider.CustomUtilities.Enums;
+
+namespace AutoBuildPredicate.PredicateSearchProvider.Helpers
+{
+    public class ExpressionDateTimeInfo
+    {
+        public ConstantExpression Constant { get; set; }
+        public CompareExpressionType? ExpressionType { get; set; }
+        public DateTime? DateTime { get; set; }
+    }
+}
